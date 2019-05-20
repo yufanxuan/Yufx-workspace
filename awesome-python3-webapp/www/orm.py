22e9d2e1be64a5a16a3d9345138121123e4746b3
yufanxuan <apple@appledeMacBook-Pro.local>
2019-05-20 23:37:06 +0800

    day 3

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -140,10 +140,102 @@ class ModelMetaclass(type):
         return type.__new__(cls, name, bases, attrs)
 
 
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise Exception(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s:%s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
 
 
+@classmethod
+async def findAll(cls, where=None, args=None, **kw):
+    ' find objects by where clause. '
+    sql = [cls.__select__]
+    if where:
+        sql.append('where')
+        sql.append(where)
+    if args is None:
+        args = []
+    orderBy = kw.get('orderBy', None)
+    if orderBy:
+        sql.append('order by')
+        sql.append(orderBy)
+    limit = kw.get('limit', None)
+    if limit is not None:
+        sql.append('limit')
+        if isinstance(limit, int):
+            sql.append('?')
+            args.append(limit)
+        elif isinstance(limit, tuple) and len(limit) == 2:
+            sql.append('?, ?')
+            args.extend(limit)
+        else:
+            raise ValueError('Invaild limit value:%s' % str(limit))
+    rs = await select(' '.join(sql), args)
+    return [cls(**r) for r in rs]
 
 
+@classmethod
+async def findNumber(cls, selectField, where=None, args=None):
+    ' find number by select and where. '
+    sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+    if where:
+        sql.append('where')
+        sql.append(where)
+    rs = await select(' '.join(sql), args, 1)
+    if len(rs) == 0:
+        return None
+    return rs[0]['_num_']
+
+
+@classmethod
+async def find(cls, pk):
+    ' find object by primary key '
+    rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+    if len(rs) == 0:
+        return None
+    return cls(**rs[0])
+
+
+async def save(self):
+    args = list(map(self.getValueOrDefault, self.__fields__))
+    args.append(self.getValueOrDefault(self.__primary_key__))
+    rows = await execute(self.__insert__, args)
+    if rows != 1:
+        logging._warn('failed to insert record: affected rows:%s' % rows)
+
+
+async def update(self):
+    args = list(map(self.getValue, self.__field__))
+    args.append(self.getValue(self.__primary_key__))
+    rows = await execute(self.__update__, args)
+    if rows != 1:
+        logging._warn('failed to update by primary key: affected rows:%s' % rows)
+
+
+async def remove(self):
+    args = [self.getValue(self.__primary_key__)]
+    rows = await execute(self.__delete__, args)
+    if rows != 1:
+        logging._warn('failed to remove by primary key: affected rows:%s' % rows)
 
 
 
